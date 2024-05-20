76e558e19056ac9a2d9d6333352f7e3d70524180
JOOMUUSUU <jnwongsa@gmail.com>
2024-05-20 10:52:29 +0700

    update:ex 1-2

--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,9 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+
+  const count = array.filter(operation).length;
+  return count >= 5;
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -10,6 +13,10 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, score => score > 70);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, score => score > 70);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, score => score > 70);
+
+console.log("นักเรียนห้องที่ 1:", scoreRoom1Result ? "ผ่านเกณฑ์" : "ไม่ผ่านเกณฑ์");
+console.log("นักเรียนห้องที่ 2:", scoreRoom2Result ? "ผ่านเกณฑ์" : "ไม่ผ่านเกณฑ์");
+console.log("นักเรียนห้องที่ 3:", scoreRoom3Result ? "ผ่านเกณฑ์" : "ไม่ผ่านเกณฑ์");

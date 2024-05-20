04c2c07e7b129dcd7a7cd4ea8a813f02b21ab0ea
JOOMUUSUU <jnwongsa@gmail.com>
2024-05-20 11:58:22 +0700

    Update:ex1-2

--- ex-1.js
+++ ex-1.js
@@ -1,12 +1,11 @@
-// Exercise #1: For Each Function
+//EX1
+let employeeSalaries = [20005,40000,32000,14500,344000];
 
-function forEach(array, operation) {
-  // Start coding here
+salariesPlus = (money,callback) => {
+    return money = money.map(callback)
 }
 
-const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
-const newEmployeeSalaries = [];
+moneyUp = (value) =>  value + 5000;
 
-// Using `forEach` function here
-
-console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
+employeeSalaries = salariesPlus(employeeSalaries,moneyUp)
+console.log(employeeSalaries)
